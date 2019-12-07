93744e83444bda89639e50e379449ce242506251
ppouch <tmdudydude@hotmail.com>
2019-12-07 11:58:20 -0500

    update

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,10 +4,16 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    SetupGame();
+
+    PrintLine(FString::Printf(TEXT("The Hidden Word is : %s"), *HiddenWord)); //Debug Line
+
     PrintLine(TEXT("Thank you for playing Bull Cows"));
     PrintLine(TEXT("Guess the 4 letter word")); // Magic number REMOVE!
-    HiddenWord = TEXT("cake");
+    
 
+    
     // Set lives
 
     // Prompt player for guess
@@ -15,11 +21,16 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    // Check if right number of characters
+    ClearScreen();
+    
+    if (Input.Len() != HiddenWord.Len())
+    {
+        PrintLine(TEXT("Wrong number of letters entered. Make sure you have 4 letters")); // Magic Number
+    }
     // Check if isogram
     // 
     
-    ClearScreen();
+    
     FString HiddenWord = TEXT("cake"); // Move outside this function
     if (Input == HiddenWord)
     {
@@ -30,10 +41,16 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
         // Remove a life
         // if still alive
         PrintLine(TEXT("You lost"));
-        PrintLine(TEXT("The hidden word was : "));
+        PrintLine(TEXT("The hidden word was : cake")); // Magic Word
     }
     
     
     FString Isogram = TEXT("oilpce");
     PrintLine(Input);
+}
+
+void UBullCowCartridge::SetupGame()
+{
+    HiddenWord = TEXT("cake");
+    Lives = 4;
 }
